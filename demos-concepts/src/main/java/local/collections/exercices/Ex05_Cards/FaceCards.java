a6625e9a521707965f915fd08bfc0321c9507f7b
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-07-17 13:44:59 +0200

    Solve collections exercise: Cards

---
+++ demos-concepts/src/main/java/local/collections/exercices/Ex05_Cards/FaceCards.java
@@ -0,0 +1,12 @@
+package local.collections.exercices.Ex05_Cards;
+
+public class FaceCards {
+
+    public String icon;
+
+    public static FaceCards[] values() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'values'");
+    }
+
+}
