eedfcff055ca06a50f3dc3635761b1b7f176de76
Evelyn Aguilar <maryeliaguilar26@gmail.com>
2025-07-23 12:40:08 +0200

    Update json

---
+++ demos-persis/src/main/java/com/cdsb/serial/JsonProcessingException.java
@@ -0,0 +1,10 @@
+package com.cdsb.serial;
+
+public class JsonProcessingException {
+
+    public void printStackTrace() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printStackTrace'");
+    }
+
+}
